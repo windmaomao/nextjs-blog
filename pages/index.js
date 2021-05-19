4b9197bc96a58478b7774712f550633ca444b1d8
Fang Jin <windmaomao@gmail.com>
2021-05-19 15:30:50 -0400

    add dynamic api

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Layout from '../components/layout'
-import Error from 'next/error'
 
 export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Home</title>
+        <title>Windmaomao</title>
       </Head>
 
       <main>     
@@ -19,18 +18,7 @@ export default function Home() {
         </p>
 
       </main>
-
-      <Error statusCode={404} />
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>      
+   
     </Layout>
   )
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,22 +1,22 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
 
-function FirstPost({ count }) {
+function FirstPost({ user }) {
   return (
     <Layout>
       <Head>
         <title>My First Post</title>
       </Head>
       <h1>First Post 2</h1>
-      <p>{count}</p>
+      <p>{user.name}</p>
     </Layout>
   )
 }
 
 FirstPost.getInitialProps = async () => {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const data = await res.json()
-  return { count: data.network_count }
+  const res = await fetch('http://localhost:3000/api/user')
+  const user = await res.json()
+  return { user }
 }
 
 export default FirstPost
