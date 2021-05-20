bea36fcf39b7234fe9f1339561992de6b5dce843
Fang Jin <windmaomao@gmail.com>
2021-05-20 14:46:56 -0400

    add next auth

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,12 +1,41 @@
-import { useContext } from 'react'
-import Config from '../Config'
+import { signIn, signOut, useSession } from 'next-auth/client'
+import styles from './avatar.module.css'
 
 function Avatar() {
-  const { user } = useContext(Config)
+  const [ session, loading ] = useSession()
+  const onSignInClick = e => {
+    e.preventDefault()
+    signIn()
+  }
+  const onSignoutClick = e => {
+    e.preventDefault()
+    signOut()
+  }
+  const { name, image } = session ? session.user : {}
   return (
-    <span>
-      {user.name}
-    </span>
+    <div>
+      {!session ? (
+        <a
+          href={`/api/auth/signin`}
+          onClick={onSignInClick}
+        >
+          Sign in
+        </a>
+      ) : (
+        <>
+          <span
+            style={{backgroundImage: `url(${image})` }}
+            className={styles.avatar}
+          ></span>&nbsp;
+          <a
+            href={`/api/auth/signout`}
+            onClick={onSignoutClick}
+          >
+            Sign out
+          </a>
+        </>
+      )}
+    </div>
   )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import './_app.css'
 import Config from '../Config'
+import { Provider as AuthProvider } from 'next-auth/client'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Config.Provider value={{ user: { name: 'John' }}}>
-      <Component {...pageProps} />
-    </Config.Provider>
+    <AuthProvider session={pageProps.session}>
+      <Config.Provider value={{ user: { name: 'John' }}}>
+        <Component {...pageProps} />
+      </Config.Provider>
+    </AuthProvider>
   )
 }
